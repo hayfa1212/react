362e20395c1da4586ec07edef820360991dd40c0
hayfa1212 <amrihaifa3@gmail.com>
2024-03-11 15:52:07 +0100

    pfe

--- gestin-stock/src/Components/siginCommun/inputForm.tsx
+++ gestin-stock/src/Components/siginCommun/inputForm.tsx
@@ -1,59 +1,70 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import google from '../../Assets/google.png';
-import './inputForm.css';
+import './inputForm.css'
 import { toast } from 'react-toastify';
 import supabase from '../../utils/api';
 
+
+
 interface MyFormProps {
     children?: ReactNode; 
-    nameButton?: string;
+    nameButton?:string;
+   
+}
+interface MyFormValues{
+    email:string;
+    password:string;
+    name:string
 }
 
-interface MyFormValues {
-    email: string;
-    password: string;
-}
-
-export const  MyForm: React.FC<MyFormProps> =  ({ children, nameButton }: MyFormProps) => {
-    const initialValues: MyFormValues = {
+export const  MyForm: React.FC<MyFormProps> =  ({ children ,nameButton}:MyFormProps) => {
+    const initialValues : MyFormValues={
         email: '',
-        password: ''
+        password: '',
+        name: ''
     };
+    
 
-    const [error, setError] = useState<string | null>(null);
-    const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-    const handleSubmit = async (values: MyFormValues) => {
-        try {
-            const { data, error } = await supabase.auth.signUp({
-                email: values.email,
-                password: values.password
-            });
-
-            if (error) {
-                setError(error.message);
-            } else {
-                setSuccessMessage('Account created successfully!');
-            }
-        } catch (error) {
-            setError('An error occurred while creating the account.');
-        }
-    };
-
+    
     return (
         <div>
+
             <Formik
                 initialValues={initialValues}
                 validationSchema={Yup.object().shape({
+                    name: Yup.string().required(),
                     email: Yup.string().email().required(),
                     password: Yup.string().min(8, 'Password must be at least 8 characters long').required()
+                  
+                    
                 })}
-                onSubmit={handleSubmit}
+
+                
+
+
+
+                onSubmit={async (values) => {
+                      
+       const { data, error } = await supabase.auth.signUp({
+         email: values?.email,
+          password: values?.password,
+          
+    
+        })
+
+                    console.log('form submit', values);
+                   console.log({data})
+                }}
             >
+                
                 {({ errors, touched }) => (
                     <Form className='inputForm'>
+
+
+                      
                         <div>
                             <label htmlFor="email">Email</label>
                             <Field type="email" name="email" className='put' placeholder="Entrez votre email" />
@@ -61,12 +72,12 @@ export const  MyForm: React.FC<MyFormProps> =  ({ children, nameButton }: MyForm
                         <div>
                             <label htmlFor="password">Password</label>
                             <Field type="password" name="password" className='put' placeholder="Create a password" />
-                            {errors.password && <p className='erreurMsg'>{errors.password}</p>}
+                            {errors.password && <p className='erreurMsg'>{errors?.password}</p>}
                         </div>
                         <div>{children}</div>
                         <div className='button'>
                             <button className='start' type="submit" >{nameButton}</button>
-                            <button className='emailButton'><img src={google} className='google' />Sign up with Google</button>
+                            <button className='emailButton'><img src={google} className='google' />Sigin up with Google</button>
                         </div>
                     </Form>
                 )}
@@ -74,3 +85,4 @@ export const  MyForm: React.FC<MyFormProps> =  ({ children, nameButton }: MyForm
         </div>
     );
 };
+
